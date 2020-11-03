e86e7983f0ecde2b2720e0cbbe9113d4742c1a24
Stacy <skim10@stuy.edu>
2020-11-02 22:47:54 -0500

    Added resize and modified add

--- SuperArray.java
+++ SuperArray.java
@@ -2,7 +2,7 @@ public class SuperArray{
   private String[] data;
   private int size;
   public SuperArray(){
-    data = new String[10];
+    data = new String[6];
   }
   public int size(){
     int count = 0;
@@ -15,6 +15,7 @@ public class SuperArray{
     return size;
   }
   public boolean add(String element){
+    if (data.length == size) resize();
     data[size] = element;
     size += 1;
     return true;
@@ -25,5 +26,12 @@ public class SuperArray{
   public String set(int index, String element){
     data[index] = element;
     return element;
+  }
+  private void resize(){
+    String[] arr = new String[data.length + 10];
+    for (int i = 0; i < size; i++){
+      arr[i] = data[i];
+    }
+    data = arr;
   }
 }
--- Tester.java
+++ Tester.java
@@ -1,6 +1,7 @@
 public class Tester{
   public static void main(String[] args){
     SuperArray words = new SuperArray();
+    //Testing size, add, and get
     System.out.println(words.size());   //0
     words.add("kani");
     words.add("uni");
@@ -8,11 +9,11 @@ public class Tester{
     words.add("abc");
     words.add("def");
     words.add("ghi");
-    System.out.println(words.size());
+    System.out.println(words.size());   //6
     for (int i = 0; i < words.size(); i++){
       System.out.println(words.get(i));
     }
-
+    //Testing set
     System.out.println("");
     words.set(3, "rst");
     words.set(4, "uvw");
@@ -20,5 +21,9 @@ public class Tester{
     for (int i = 0; i < words.size(); i++){
       System.out.println(words.get(i));
     }
+    //Testing resize and modified add
+    words.add("hij");
+    words.add("klm");
+    System.out.println(words.size());   //8
   }
 }
